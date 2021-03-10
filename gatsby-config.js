7ef1bc3b62720a4f60793922da54a3ea9ac53dfc
Eminakkoc <emin.akkoc@gmail.com>
2021-03-10 14:05:48 +0300

    add watch mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         projectId: `ngnlewza`,
         dataset: `production`,
         overlayDrafts: true,
+        watchMode: true,
         token: `skIZdmVjHshQTMVKVqAOqbDXP2IwoB3uCgvOy7IEdYxoUiVwOD29NtMZ8cqWgmzWtvsR9fKfVeN4XQgHzp0kZ4hCEzY8XM6vpfthrI48Wqls4AupENMWLfT3fDMGrJ7OocVe0syWUVY9dgI1zp9DBkIwnjJBxTYvtValawtlHWlynPVW03v0`
       },
     },
